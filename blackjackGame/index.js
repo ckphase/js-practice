56eaa813a4b8b9e977a72d78925b0912a88a5e45
ckphase <worldchampion202428@gmail.com>
2025-09-07 14:18:19 +0530

    Created CounterApp, Practice 1, Practice 1 part 2

---
+++ blackjackGame/index.js
@@ -0,0 +1,90 @@
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = " "
+let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+// random number generation 
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()*13)+1
+    // if 1     -> return 11
+    // if 11-13 -> return 10
+    if (randomNumber === 1){
+        return 11
+    } else if(randomNumber  > 10){
+        return 10
+    } else {
+        return randomNumber
+    }
+
+}
+
+function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let SecondCard = getRandomCard()
+    cards = [firstCard, SecondCard]
+    sum = firstCard + SecondCard
+    rendergame()
+}
+
+function rendergame(){
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? "
+    } else if (sum === 21) {
+        hasBlackJack = true
+        message = "Wohoo! You've got Blackjack! "
+    } else {
+        isAlive = false
+        message = "You're out of the game! "
+    }
+
+    console.log(message)
+    messageEl.textContent = message
+}
+
+function newCard(){
+    if(isAlive === true && hasBlackJack === false){
+    console.log("Drawing new card form the deck!")
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    rendergame()
+    }
+}
+
+// // 3. Log it out to check that you're doing it right
+// if (isAlive){
+//     console.log("You are still in the game")
+// }else{
+//     console.log("You are out of the game")
+// }
+// // Check if the person is old enough to enter the nightclub (21)
+// // Log a suitable message to the console in both cases
+
+// let age = 22
+
+// // if less than 21 -> "You can not enter the club!"
+// if (age > 21){
+//     console.log("Welcome")
+// }else {
+//     console.log("You can not enter the club!")
+// }
+
+// console.log(4 === 3)  //false 
+// console.log(5 > 2)    // true
+// console.log(12 > 12)  //false
+// console.log(3 < 0)    //false
+// console.log(3 >= 3)   //true
+// console.log(11 <= 11) //true
+// console.log(3 <= 2)   //false
---
+++ practice1/index.js
@@ -0,0 +1,55 @@
+// Create two variables, firstName and lastName
+let firstName = "Jimin"
+let lastName = "Park"
+// Concatenate the two variables into a third variable called fullName
+let concatenate = firstName + " " + lastName
+// Log fullName to the console
+console.log(concatenate)
+
+
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greetingLinda(){
+    console.log(greeting + ", " + name + "!")
+}
+greetingLinda()
+
+
+let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+function add3Points(){
+    myPoints += 3
+}
+function remove1Point(){
+    myPoints -= 1
+}
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+// Call the functions to that the line below logs out 10
+console.log(myPoints)
+
+
+// Try to predict what each of the lines will log out
+console.log("2" + 2) //22
+console.log(11 + 7) //18
+console.log(6 + "5") //65
+console.log("My points: " + 5 + 9) //My points: 59
+console.log(2 + 2) // 4
+console.log("11" + "14") // 1114
+
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+let errorPara = document.getElementById("error")
+function errorMessage(){
+    console.log("click")
+    errorPara.textContent = "Something went wrong, please try again"
+}
