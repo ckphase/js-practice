7b14cd3dd85cc0374b90d644e5915f632607ff2f
ckphase <worldchampion202428@gmail.com>
2025-09-09 22:28:35 +0530

    Update: Chrome Extension deployed to chrome

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,13 +1,21 @@
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-console.log(localStorage.getItem("myLeads"))
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+
+
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    console.log(localStorage.getItem("myLeads"))
+
     render()
 })
 
