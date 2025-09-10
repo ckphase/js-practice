4eb40ded6ad33de7c45ecf89b8600782c3074330
ckphase <worldchampion202428@gmail.com>
2025-09-10 23:24:37 +0530

    Update index.js'

--- linkioMobileApp/index.js
+++ linkioMobileApp/index.js
@@ -1,18 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
-import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDkiWTDOprR7UZuF9cHfWopmTtRS_QGaCI",
-  authDomain: "urlio-96489.firebaseapp.com",
-  databaseURL: "https://urlio-96489-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "urlio-96489",
-  storageBucket: "urlio-96489.firebasestorage.app",
-  messagingSenderId: "832322716860",
-  appId: "1:832322716860:web:e630f2721385ca0aa2bcb9",
-  measurementId: "G-MVT2D24MX6"
-};
-
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
